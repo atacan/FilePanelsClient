92bf48db4e7be0ea32bbece0e7b1767ef838b94c
atacan <atacandur@icloud.com>
2023-09-17 12:05:36 +0200

    initial

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "FilePanelsClient",
+    platforms: [
+        .macOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FilePanelsClient",
             targets: ["FilePanelsClient"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FilePanelsClient"),
+            name: "FilePanelsClient", dependencies: [
+                .product(name: "Dependencies", package: "swift-dependencies"),
+            ]),
         .testTarget(
             name: "FilePanelsClientTests",
             dependencies: ["FilePanelsClient"]),
